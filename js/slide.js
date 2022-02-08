3c3fd939f6b0c0b9d655030775873e8a8849be6a
Hyeongyun <shg7271@gmail.com>
2022-02-08 21:47:52 +0900

    Update: bl_projects slide js window resize responsive work

--- js/slide.js
+++ js/slide.js
@@ -7,37 +7,99 @@
 
   // 슬라이드 시 조정되는 오프셋
   let leftOffset = 0;
+  let MAX_SLIDE = 3;
+  let slideTotalWidth = slide_items.clientWidth;
+  let slideWidth = slideTotalWidth / MAX_SLIDE;
+  let center = 2;
+  let currShow = 0;
   // max offset 최대 오프셋
-  const MAX_SLIDE = 3;
-  const MAX_OFFSET = (slide_items.childElementCount - MAX_SLIDE) * 300;
+  const slideTotalCount = slide_items.childElementCount;
+  let MAX_OFFSET = (slide_items.childElementCount - MAX_SLIDE) * slideWidth;
+  // console.log(MAX_OFFSET);
   // console.log(slide_items.childElementCount);
   // console.log(slide_items.children, slide_items.children[0]);
 
   // for (let i = 0; i < MAX_SLIDE; i++) {
   //   console.log(slide_items.children[i].classList.add('show'));
   // }
+  {
+    // 슬라이드 초기값 설정
+
+    // console.log(window.innerWidth);
+    if (window.innerWidth >= 1240) {
+      MAX_SLIDE = 3;
+      slideWidth = slideTotalWidth / MAX_SLIDE;
+      if (currShow === slideTotalCount - 2) currShow -= 1;
+    } else if (window.innerWidth >= 768) {
+      MAX_SLIDE = 2;
+      slideWidth = slideTotalWidth / MAX_SLIDE;
+      if (currShow === slideTotalCount - 1) currShow -= 1;
+    } else {
+      MAX_SLIDE = 1;
+      slideWidth = slideTotalWidth / MAX_SLIDE;
+    }
+    slide_item.forEach((item) => {
+      // console.log(item.style.width);
+      item.style.width = slideWidth + 'px';
+      // item.style.left = leftOffset + 'px'
+    })
+  }
+
+  // console.log(slideTotalWidth / 3);
+  // 윈도우 크기에 따라 슬라이드 반응형으로 사이즈 조절
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 1240) {
+      MAX_SLIDE = 3;
+      if (currShow === slideTotalCount - 2) currShow -= 1;
+    } else if (window.innerWidth >= 768) {
+      MAX_SLIDE = 2;
+      if (currShow === slideTotalCount - 1) currShow -= 1;
+    } else {
+      MAX_SLIDE = 1;
+    }
+    slideTotalWidth = slide_items.clientWidth;
+    slideWidth = slideTotalWidth / MAX_SLIDE;
+    MAX_OFFSET = (slide_items.childElementCount - MAX_SLIDE) * slideWidth;
+    console.log(`currShow: ${currShow}`);
+    leftOffset = -(slideWidth * currShow);
+
+    // 윈도우 크기에 따라 슬라이드 item의 width와 leftOffset 재설정
+    slide_item.forEach((item) => {
+      // console.log(item.style.width);
+      item.style.width = slideWidth + 'px';
+      item.style.left = leftOffset + 'px'
+    })
+    // console.log(slideTotalWidth / 3);
+  })
+  // slide_item.forEach((item) => {
+  //   item.style.width =
+  // })
 
   // slide move function decalration
   function leftMove() {
-    leftOffset += 300;
-    slide_item.forEach((item) => {
-      if (leftOffset <= 0) {
+    leftOffset += slideWidth;
+    currShow -= 1;
+    if (leftOffset <= 0) {
+      slide_item.forEach((item) => {
         item.style.left = leftOffset + 'px';
-      } else {
-        leftOffset -= 300;
-      }
-    });
+      });
+    } else {
+      leftOffset -= slideWidth;
+      currShow += 1;
+    }
   }
 
   function rigthMove() {
-    leftOffset -= 300;
-    slide_item.forEach((item) => {
-      if (leftOffset >= -MAX_OFFSET) {
+    leftOffset -= slideWidth;
+    currShow += 1;
+    if (leftOffset >= -MAX_OFFSET) {
+      slide_item.forEach((item) => {
         item.style.left = leftOffset + 'px';
-      } else {
-        leftOffset += 300;
-      }
-    });
+      });
+    } else {
+      leftOffset += slideWidth;
+      currShow -= 1;
+    }
   }
 
   // pc, mobile 좌 우 버튼 슬라이드 기능
