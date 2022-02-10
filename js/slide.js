a131bec1cb299e835603abd14a487d9c5fad779d
Hyeongyun <shg7271@gmail.com>
2022-02-10 22:35:49 +0900

    Update: bl_projects_slide filter function - add tag when change select filter - remove tag when click tag close btn - change & resize slide by filter

--- js/slide.js
+++ js/slide.js
@@ -3,18 +3,19 @@
   const slide_items = document.querySelector('.bl_projects_slide_items');
   const leftArrow = document.querySelector('.el_leftArrow');
   const rightArrow = document.querySelector('.el_rightArrow');
-  const slide_item = document.querySelectorAll('.bl_projects_slide_item');
-
+  let slide_item = document.querySelectorAll('.bl_projects_slide_item.show');
+  console.log(slide_item);
   // 슬라이드 시 조정되는 오프셋
   let leftOffset = 0;
   let MAX_SLIDE = 3;
-  let slideTotalWidth = slide_items.clientWidth;
-  let slideWidth = slideTotalWidth / MAX_SLIDE;
+  let slideContainerWidth = slide_items.clientWidth;
+  let slideWidth = slideContainerWidth / MAX_SLIDE;
   let center = 2;
   let currShow = 0;
   // max offset 최대 오프셋
-  const slideTotalCount = slide_items.childElementCount;
-  let MAX_OFFSET = (slide_items.childElementCount - MAX_SLIDE) * slideWidth;
+  console.log('slide_item length', slide_item.length);
+  let slideTotalCount = slide_item.length;
+  let MAX_OFFSET = (slideTotalCount - MAX_SLIDE) * slideWidth;
   // console.log(MAX_OFFSET);
   // console.log(slide_items.childElementCount);
   // console.log(slide_items.children, slide_items.children[0]);
@@ -22,55 +23,47 @@
   // for (let i = 0; i < MAX_SLIDE; i++) {
   //   console.log(slide_items.children[i].classList.add('show'));
   // }
-  {
-    // 슬라이드 초기값 설정
-
-    // console.log(window.innerWidth);
+  function setSlideSize(init = false) {
+    slide_item = document.querySelectorAll('.bl_projects_slide_item.show');
     if (window.innerWidth >= 1240) {
       MAX_SLIDE = 3;
-      slideWidth = slideTotalWidth / MAX_SLIDE;
-      if (currShow === slideTotalCount - 2) currShow -= 1;
-    } else if (window.innerWidth >= 768) {
+      // slideWidth = slideContainerWidth / MAX_SLIDE;
+      if (slideTotalCount > 2 && currShow === slideTotalCount - 2) currShow -= 1;
+    } else if (slideTotalCount > 1 && window.innerWidth >= 768) {
       MAX_SLIDE = 2;
-      slideWidth = slideTotalWidth / MAX_SLIDE;
+      // slideWidth = slideContainerWidth / MAX_SLIDE;
       if (currShow === slideTotalCount - 1) currShow -= 1;
     } else {
       MAX_SLIDE = 1;
-      slideWidth = slideTotalWidth / MAX_SLIDE;
     }
+
+    slideTotalCount = slide_item.length;
+    slideContainerWidth = slide_items.clientWidth;
+    slideWidth = slideContainerWidth / MAX_SLIDE;
+    MAX_OFFSET = (slideTotalCount - MAX_SLIDE) * slideWidth;
+    leftOffset = -(slideWidth * currShow);
+    if (init === true) {
+      leftOffset = 0;
+      currShow = 0;
+    }
+
     slide_item.forEach((item) => {
-      // console.log(item.style.width);
       item.style.width = slideWidth + 'px';
-      // item.style.left = leftOffset + 'px'
-    })
+      item.style.left = leftOffset + 'px';
+    });
+  }
+  {
+    // DOM 최초 로드 시 슬라이드 사이즈 세팅
+    document.addEventListener('DOMContentLoaded', () => {
+      setSlideSize();
+    });
   }
 
-  // console.log(slideTotalWidth / 3);
+  // console.log(slideContainerWidth / 3);
   // 윈도우 크기에 따라 슬라이드 반응형으로 사이즈 조절
   window.addEventListener('resize', () => {
-    if (window.innerWidth >= 1240) {
-      MAX_SLIDE = 3;
-      if (currShow === slideTotalCount - 2) currShow -= 1;
-    } else if (window.innerWidth >= 768) {
-      MAX_SLIDE = 2;
-      if (currShow === slideTotalCount - 1) currShow -= 1;
-    } else {
-      MAX_SLIDE = 1;
-    }
-    slideTotalWidth = slide_items.clientWidth;
-    slideWidth = slideTotalWidth / MAX_SLIDE;
-    MAX_OFFSET = (slide_items.childElementCount - MAX_SLIDE) * slideWidth;
-    console.log(`currShow: ${currShow}`);
-    leftOffset = -(slideWidth * currShow);
-
-    // 윈도우 크기에 따라 슬라이드 item의 width와 leftOffset 재설정
-    slide_item.forEach((item) => {
-      // console.log(item.style.width);
-      item.style.width = slideWidth + 'px';
-      item.style.left = leftOffset + 'px'
-    })
-    // console.log(slideTotalWidth / 3);
-  })
+    setSlideSize();
+  });
   // slide_item.forEach((item) => {
   //   item.style.width =
   // })
@@ -149,55 +142,86 @@
 }
 
 {
-  const slide_items = document.querySelector('.bl_projects_slide_items');
-  const slide_item = document.querySelectorAll('.bl_projects_slide_item');
+  const selectDOM = document.querySelector(
+    ".bl_projects_filters > select"
+  );
+  const tagsDOM = document.querySelector(".bl_projects_filters_tags");
+  const elem3 = document.querySelectorAll(".el_tag > .js_close");
+  // console.log(elem.selected);
+  selectDOM.addEventListener("change", function () {
+    // console.log(this.options[this.selectedIndex].value);
+    // 변경으로 선택된 값
+    let val = this.options[this.selectedIndex].value;
+    console.log(this.selectedIndex);
+    // 생성할 태그 앨리먼트
+    let innerElem = document.createElement("span");
+    innerElem.innerHTML = `
+            <span class="el_tag"
+            ><span>${val}</span
+            ><img src="./img/close_white_24dp.svg" alt=""
+          /></span>
+          `;
+    // tags에 생성한 태그 앨리먼트 추가
+    if (!getFilters().includes(val)) {
+      tagsDOM.appendChild(innerElem);
+    }
 
-  // 모바일에서만 터치 이벤트 적용
-  let start = 0;
-  let end = 0;
-  let leftOffset = 0;
-  // slide_items.addEventListener('touchstart', (e) => {
-  //   console.log(e.touches[0].pageX);
-  //   start = e.touches[0].pageX;
-  //   console.log(start);
-  // })
-  // slide_items.addEventListener('touchend', (e) => {
+    // console.log(innerElem.childNodes[1].childNodes[1]);
+    // 변경된 값에 대해 기능 필요
+    getSlides();
+    innerElem.childNodes[1].childNodes[1].addEventListener(
+      "click",
+      () => {
+        innerElem.remove();
+        // 변경된 값에 대해 기능 필요
+        getSlides();
+      }
+    );
+    // 필터 선택으로 변경
+    this.selectedIndex = 0;
+  });
+  elem3.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.parentElement.remove();
+    });
+  });
 
-  //   console.log(e.changedTouches[0].pageX);
-  //   end = e.changedTouches[0].pageX;
-  //   if ((end - start) > 0) {
-  //     leftOffset += 300;
-  //   console.log(leftOffset);
-  //   slide_item.forEach((item) => {
-  //     if (leftOffset <= 0) {
-  //       item.style.left = leftOffset + 'px';
-  //     } else {
-  //       leftOffset -= 300;
-  //     }
-  //   })
-  //   }
-  // })
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  function getFilters() {
+    const filtering = document.querySelectorAll(".el_tag > span");
+    let arr = [];
+    filtering.forEach((item) => {
+      // console.log(a);
+      // console.log(item.innerHTML);
+      arr.push(item.innerHTML);
+    });
+    return arr;
+  }
+  // let b = getFilters();
+  // console.log(b);
+  function getSlides() {
+    const slideItem = document.querySelectorAll(
+      ".bl_projects_slide_item"
+    );
+    slideItem.forEach((item) => {
+      let slideTags = String(item.dataset["tag"])
+        .split(",")
+        .map((i) => i.trim());
+      // console.log(
+      //   slideTags.filter((i) => getFilters().includes(i)).length ===
+      //   getFilters().length;
+      // );
+      // console.log(getFilters());
+      if (
+        slideTags.filter((i) => getFilters().includes(i)).length ===
+        getFilters().length
+      ) {
+        item.classList.add("show");
+      } else {
+        item.classList.remove("show");
+      }
+    });
 
-  // slide_items.addEventListener('mousedown', (e) => {
-  //   console.log(`start: ${e.pageX}`);
-  //   start = e.pageX;
-  // })
-  // slide_items.addEventListener('mousemove', (e) => {
-  //   // console.log(e.pageX);
-  // })
-  // slide_items.addEventListener('mouseup', (e) => {
-  //   console.log(`end: ${e.pageX}`);
-  //   end = e.pageX;
-  //   if (start > end) {
-  //     leftOffset -= 300;
-  //     console.log(leftOffset);
-  //     slide_item.forEach((item) => {
-  //       if (leftOffset >= -MAX_OFFSET) {
-  //         item.style.left = leftOffset + 'px';
-  //       } else {
-  //         leftOffset += 300;
-  //       }
-  //     })
-  //   }
-  // })
+    setSlideSize(true);
+  }
 }
