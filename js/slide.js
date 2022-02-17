f46d00e695c56390543912cad1d37f7ec7dcdb7e
Hyeongyun <shg7271@gmail.com>
2022-02-17 22:31:52 +0900

    Update: add function that delete all selected tag - add try catch syntax to add DOM eventListener(fix null error)

--- js/slide.js
+++ js/slide.js
@@ -25,16 +25,14 @@
         `${this.slideSelector} .el_select .el_select_options`,
       );
       this.filter.forEach(i => {
-        // console.log([...i.children]);
         [...i.children].forEach(i2 => {
           i2.addEventListener('click', () => {
-            // console.log(i2);
             i2.classList.toggle('js_check');
             const innerElem = document.createElement('span');
             innerElem.innerHTML = `
               <span>${i2.innerHTML}</span>
               <img
-                src="./img/close_white_24dp.svg"
+                src="./img/close_black_24dp.svg"
                 alt="close button"
                 class="js_close"
               />
@@ -49,13 +47,13 @@
             if (i2.classList.contains('js_check')) {
               // 추가
               document
-                .querySelector('.bl_projects_filters_tags')
+                .querySelector(`${this.slideSelector}_tags`)
                 .append(innerElem);
               this.getSlides();
             } else {
               // 삭제
               document
-                .querySelectorAll('.bl_projects_filters_tags > .el_tag > span')
+                .querySelectorAll(`${this.slideSelector}_tags > .el_tag > span`)
                 .forEach(i3 => {
                   if (i3.innerHTML.trim() === i2.innerHTML.trim()) {
                     i3.parentElement.remove();
@@ -66,6 +64,25 @@
           });
         });
       });
+      this.deleteAll = document.querySelector(
+        `${this.slideSelector} .un_deleteAll`,
+      );
+      try {
+        this.deleteAll.addEventListener('click', () => {
+          document.querySelectorAll('.bl_projects_tags > span').forEach(ii => {
+            ii.remove();
+          });
+          this.filter.forEach(jj => {
+            [...jj.children].forEach(kk => {
+              kk.classList.remove('js_check');
+            });
+          });
+          this.getSlides();
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
       // variable init
       this.leftOffset = 0;
       this.maxSlide = 3;
@@ -77,21 +94,32 @@
         (this.slideTotalCount - this.maxSlide) * this.slideItemWidth;
       this.start = 0;
       this.end = 0;
-      this.leftArrow.style.top = `${
-        this.slideContainer.clientHeight / 2 - this.leftArrow.clientHeight / 2
-      }px`;
-      this.rightArrow.style.top = `${
-        this.slideContainer.clientHeight / 2 - this.rightArrow.clientHeight / 2
-      }px`;
+      try {
+        this.leftArrow.style.top = `${
+          this.slideContainer.clientHeight / 2 - this.leftArrow.clientHeight / 2
+        }px`;
+        this.rightArrow.style.top = `${
+          this.slideContainer.clientHeight / 2 -
+          this.rightArrow.clientHeight / 2
+        }px`;
+      } catch (error) {
+        console.log(error);
+      }
+
       this.resize();
 
       // binding event handler
-      this.leftArrow.addEventListener('click', () => {
-        this.leftMove();
-      });
-      this.rightArrow.addEventListener('click', () => {
-        this.rightMove();
-      });
+      try {
+        this.leftArrow.addEventListener('click', () => {
+          this.leftMove();
+        });
+        this.rightArrow.addEventListener('click', () => {
+          this.rightMove();
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
       this.slideContainer.addEventListener('touchstart', e => {
         this.start = e.touches[0].pageX;
       });
@@ -213,7 +241,7 @@
           <div>조건에 해당하는 결과가 존재하지 않습니다.</div>
         `;
       if (this.slideTotalCount === 0) {
-        elem.classList.add('bl_projects_slide_item');
+        elem.classList.add(`${this.slideSelector.slice(1)}_slide_item`);
         elem.classList.add('show');
         elem.classList.add('noresult');
         this.slideContainer.append(elem);
@@ -243,8 +271,8 @@
   }
 
   // 인스턴스 생성 - 슬라이드 기능 구현
-  const blStacksSlide = new BlSlide('.bl_stacks');
   const blProjectsSlide = new BlSlide('.bl_projects');
+  const blStacksSlide = new BlSlide('.bl_stacks');
 
   document
     .querySelector('.el_projects_toggle')
